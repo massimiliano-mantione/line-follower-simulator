e8314c244846e5101d8776e21be224ed851d5e35
Mikle <m.michele.2003@gmail.com>
2025-10-20 22:35:10 +0200

    sim: add motor angles

--- sim/sim/src/sensors/mod.rs
+++ sim/sim/src/sensors/mod.rs
@@ -4,13 +4,19 @@ use execution_data::SensorsData;
 
 pub mod bot_position;
 pub mod line_sensors;
+pub mod motor_angles;
 
 use bot_position::compute_bot_position;
 use line_sensors::compute_sensor_readings;
+use motor_angles::compute_motor_angles_position;
 
 fn print_sensors_data(sensors_data: Res<SensorsData>) {
     println!("line sensors: {:?}", sensors_data.line_sensors);
     println!("bot position: {:?}", sensors_data.bot_position);
+    println!(
+        "motor angles: l {} r {}",
+        sensors_data.motor_angles.left, sensors_data.motor_angles.right
+    );
 }
 
 pub fn add_sensors(app: &mut App) {
@@ -19,6 +25,7 @@ pub fn add_sensors(app: &mut App) {
         (
             compute_sensor_readings,
             compute_bot_position,
+            compute_motor_angles_position,
             print_sensors_data,
         )
             .chain()
---
+++ sim/sim/src/sensors/motor_angles.rs
@@ -0,0 +1,32 @@
+use std::f32::consts::PI;
+
+use bevy::prelude::*;
+
+use crate::{
+    motors::Wheel,
+    utils::{SetBySide, Side},
+};
+use execution_data::{MotorAngles, SensorsData};
+
+impl SetBySide<f32> for MotorAngles {
+    fn set_by_side(&mut self, side: Side, value: f32) {
+        match side {
+            Side::Left => self.left = value,
+            Side::Right => self.right = value,
+        };
+    }
+}
+
+pub fn compute_motor_angles_position(
+    wheels_query: Query<(&Wheel, &Transform)>,
+    mut sensors_data: ResMut<SensorsData>,
+) {
+    for (wheel, transform) in &wheels_query {
+        // Rotation in radians [0, 2pi]
+        let rot = PI
+            - Vec3::from(transform.rotation.to_euler(EulerRot::XYZ))
+                .dot(transform.rotation * wheel.axle.abs());
+
+        sensors_data.motor_angles.set_by_side(wheel.side, rot);
+    }
+}
